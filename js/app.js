7198f397f990a89c26595a01e7d11aa9b6519589
ArrealdoRivaldi <ArrealdoRivaldi@users.noreply.github.com>
2025-04-09 10:20:31 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,22 +1,10 @@
 $(document).ready(function () {
     let table1, table2, table3;
+    let lastLoaded = { file1: "", file2: "", file3: "" };
 
     function initializeTables() {
-        table1 = $('#table1').DataTable({
-            paging: false,
-            lengthChange: false,
-            ordering: false,
-            info: false,
-            searching: false,
-            dom: 'Bfrtip',
-            buttons: [
-                'copy',
-                {
-                    extend: 'excel',
-                    text: 'Download Excel',
-                    title: 'Data Table 1'
-                }
-            ],
+        table1 = $('#table1').DataTable({ paging: false, lengthChange: false, ordering: false, info: false, searching: false, dom: 'Bfrtip',
+            buttons: ['copy', { extend: 'excel', text: 'Download Excel', title: 'Data Table 1' }],
             columns: [
                 { data: "NOP", title: "NOP" },
                 { data: "PS", title: "PS" },
@@ -33,21 +21,8 @@ $(document).ready(function () {
             ]
         });
 
-        table2 = $('#table2').DataTable({
-            paging: false,
-            lengthChange: false,
-            ordering: false,
-            info: false,
-            searching: false,
-            dom: 'Bfrtip',
-            buttons: [
-                'copy',
-                {
-                    extend: 'excel',
-                    text: 'Download Excel',
-                    title: 'Data Table 2'
-                }
-            ],
+        table2 = $('#table2').DataTable({ paging: false, lengthChange: false, ordering: false, info: false, searching: false, dom: 'Bfrtip',
+            buttons: ['copy', { extend: 'excel', text: 'Download Excel', title: 'Data Table 2' }],
             columns: [
                 { data: "Symptom", title: "Symptom" },
                 { data: "PS", title: "PS" },
@@ -64,21 +39,8 @@ $(document).ready(function () {
             ]
         });
 
-        table3 = $('#table3').DataTable({
-            paging: false,
-            lengthChange: false,
-            ordering: false,
-            info: false,
-            searching: false,
-            dom: 'Bfrtip',
-            buttons: [
-                'copy',
-                {
-                    extend: 'excel',
-                    text: 'Download Excel',
-                    title: 'Data Table 3'
-                }
-            ],
+        table3 = $('#table3').DataTable({ paging: false, lengthChange: false, ordering: false, info: false, searching: false, dom: 'Bfrtip',
+            buttons: ['copy', { extend: 'excel', text: 'Download Excel', title: 'Data Table 3' }],
             columns: [
                 { data: "Symptom", title: "Symptom" },
                 { data: "1-3 hari", title: "1-3 Hari" },
@@ -90,12 +52,17 @@ $(document).ready(function () {
         });
     }
 
-    function loadTableData(table, jsonFile) {
-        $.getJSON(jsonFile, function (response) {
-            console.log("Data dari " + jsonFile + " berhasil dimuat:", response);
+    function loadTableData(table, jsonFile, fileKey) {
+        if (lastLoaded[fileKey] === jsonFile) {
+            console.log(`Data ${jsonFile} sudah dimuat, tidak perlu muat ulang.`);
+            return;
+        }
 
+        $.getJSON(jsonFile, function (response) {
+            console.log("Memuat:", jsonFile, response);
             if (response && response.data && Array.isArray(response.data)) {
                 table.clear().rows.add(response.data).draw();
+                lastLoaded[fileKey] = jsonFile;
             } else {
                 console.error("Format JSON tidak sesuai:", response);
             }
@@ -104,37 +71,30 @@ $(document).ready(function () {
         });
     }
 
-    $('#dataSelector').on('change', function () {
+    $('#dataSelector').off('change').on('change', function () {
         let selectedOption = $(this).find(':selected');
         let file1 = selectedOption.data('table1');
         let file2 = selectedOption.data('table2');
         let file3 = selectedOption.data('table3');
 
         if (file1 && file2 && file3) {
-            // Selalu reset tabel dulu agar data lama hilang
-            table1.clear().draw();
-            table2.clear().draw();
-            table3.clear().draw();
-
-            // Load data baru dari file JSON
-            loadTableData(table1, file1);
-            loadTableData(table2, file2);
-            loadTableData(table3, file3);
+            loadTableData(table1, file1, 'file1');
+            loadTableData(table2, file2, 'file2');
+            loadTableData(table3, file3, 'file3');
         }
     });
 
-    // Inisialisasi DataTable saat halaman dimuat
     initializeTables();
 
-    // Load data default dari opsi 'April'
+    // Load data default hanya sekali saat pertama kali page load
     let defaultOption = $('#dataSelector option[value="Apr"]');
     let file1 = defaultOption.data('table1');
     let file2 = defaultOption.data('table2');
     let file3 = defaultOption.data('table3');
 
     if (file1 && file2 && file3) {
-        loadTableData(table1, file1);
-        loadTableData(table2, file2);
-        loadTableData(table3, file3);
+        loadTableData(table1, file1, 'file1');
+        loadTableData(table2, file2, 'file2');
+        loadTableData(table3, file3, 'file3');
     }
 });
