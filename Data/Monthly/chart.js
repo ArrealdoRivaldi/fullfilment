e70c9cbcff08511038078ce2993de9070910030f
ArrealdoRivaldi <ArrealdoRivaldi@users.noreply.github.com>
2025-04-16 15:09:38 +0800

    update

--- Data/Monthly/chart.js
+++ Data/Monthly/chart.js
@@ -3,34 +3,33 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
-        ['Category', 'Reg', { role: 'annotation' }, 'PS', { role: 'annotation' }, 'Target PS', { role: 'annotation' }, 'PS/Reg', { role: 'annotation' }],
-        ['1', 27, '27', 17, '17', 18, '18', 0.6477 * 80, '64.77%'],
-        ['2', 26, '26', 16, '16', 19, '19', 0.6097 * 80, '60.97%'],
-        ['3', 22, '22', 14, '14', 19, '19', 0.6379 * 80, '63.79%'],
-        ['4',  6,  '6',  4,  '4', 17, '17', 0.2463 * 80, '24.63%'],
+        ['Bulan', 'Reg', { role: 'annotation' }, 'PS', { role: 'annotation' }, 'Target PS', { role: 'annotation' }, 'PS/Reg', { role: 'annotation' }],
+        ['Januari', 27, '27', 17, '17', 18, '18', 0.6477 * 100, '64.77%'],
+        ['Februari', 26, '26', 16, '16', 19, '19', 0.6097 * 100, '60.97%'],
+        ['Maret', 22, '22', 14, '14', 19, '19', 0.6379 * 100, '63.79%'],
+        ['April', 6, '6', 4, '4', 17, '17', 0.2463 * 100, '24.63%'],
     ]);
 
     var options = {
         title: '',
         vAxes: {
-            0: {title: 'Thousands'},
-            1: {title: 'Percentage', format: 'percent', viewWindow: {min: 0, max: 1}}
+            0: {title: 'Jumlah'},
+            1: {title: 'Persentase', format: 'percent', viewWindow: {min: 0, max: 100}}
         },
         hAxis: {
-            title: 'Bulan',
-            format: 'MMM yyyy' // Contoh: Jan 2025, Feb 2025, dst.
+            title: 'Bulan'
         },
         seriesType: 'bars',
         series: {
             3: {
                 type: 'line',
-                targetAxisIndex: 0,
+                targetAxisIndex: 1,
                 lineWidth: 3,
                 pointSize: 8,
                 color: '#3498db',
                 curveType: 'none'
             }
-        },        
+        },
         annotations: {
             alwaysOutside: true,
             highContrast: true,
