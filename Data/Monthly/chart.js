2ae279a360c79038d8a4bfd32eaaa3796dab1a91
ArrealdoRivaldi <ArrealdoRivaldi@users.noreply.github.com>
2025-04-16 14:36:02 +0800

    update

--- Data/Monthly/chart.js
+++ Data/Monthly/chart.js
@@ -3,33 +3,27 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
-        ['Month', 'Reg', 'PS', 'Target PS', 'PS/Reg'],
-        ['Jan', 27, 17, 18, 0.6477],
-        ['Feb', 26, 16, 19, 0.6097],
-        ['Mar', 22, 14, 19, 0.6379],
-        ['Apr',  6,  4, 17, 0.2463],
+        ['Category', 'Reg', { role: 'annotation' }, 'PS', { role: 'annotation' }, 'Target PS', { role: 'annotation' }, 'PS/Reg', { role: 'annotation' }],
+        ['1', 27, '27', 17, '17', 18, '18', 0.6477 * 80, '64.77%'],
+        ['2', 26, '26', 16, '16', 19, '19', 0.6097 * 80, '60.97%'],
+        ['3', 22, '22', 14, '14', 19, '19', 0.6379 * 80, '63.79%'],
+        ['4',  6,  '6',  4,  '4', 17, '17', 0.2463 * 80, '24.63%'],
     ]);
 
     var options = {
         title: '',
         vAxes: {
-            0: {title: 'Jumlah'},
-            1: {title: 'PS/Reg (%)', format: 'percent', viewWindow: {min: 0, max: 1}}
+            0: {title: 'Thousands'},
+            1: {title: 'Percentage', format: 'percent', viewWindow: {min: 0, max: 1}}
         },
-        hAxis: {
-            title: 'Month',
-            slantedText: true,
-            slantedTextAngle: 45,
-            showTextEvery: 1,
-            textStyle: { fontSize: 12 }
-        },
+        hAxis: {title: ''},
         seriesType: 'bars',
         series: {
-            3: {type: 'line', targetAxisIndex: 1, color: '#3498db', lineWidth: 3, pointSize: 6}
+            3: {type: 'line', targetAxisIndex: 0, lineWidth: 3, pointSize: 8, color: '#3498db', curveType: 'none'}
         },
-        focusTarget: 'category',
         annotations: {
             alwaysOutside: true,
+            highContrast: true,
             textStyle: {
                 fontSize: 12,
                 color: '#000',
