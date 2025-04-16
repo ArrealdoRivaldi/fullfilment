b104009ef5df0ee912428f2aae61215f0edb6812
ArrealdoRivaldi <ArrealdoRivaldi@users.noreply.github.com>
2025-04-16 14:28:42 +0800

    update

--- Data/Monthly/chart.js
+++ Data/Monthly/chart.js
@@ -3,11 +3,11 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
-        ['Category', 'Reg', { role: 'annotation' }, 'PS', { role: 'annotation' }, 'Target PS', { role: 'annotation' }, 'PS/Reg', { role: 'annotation' }],
-        ['1', 27, '27', 17, '17', 18, '18', 0.6477 * 80, '64.77%'],
-        ['2', 26, '26', 16, '16', 19, '19', 0.6097 * 80, '60.97%'],
-        ['3', 22, '22', 14, '14', 19, '19', 0.6379 * 80, '63.79%'],
-        ['4',  6,  '6',  4,  '4', 17, '17', 0.2463 * 80, '24.63%'],
+        ['Month', 'Reg', { role: 'annotation' }, 'PS', { role: 'annotation' }, 'Target PS', { role: 'annotation' }, 'PS/Reg', { role: 'annotation' }],
+        ['Jan', 27, '27', 17, '17', 18, '18', 0.6477 * 80, '64.77%'],
+        ['Feb', 26, '26', 16, '16', 19, '19', 0.6097 * 80, '60.97%'],
+        ['Mar', 22, '22', 14, '14', 19, '19', 0.6379 * 80, '63.79%'],
+        ['Apr',  6,  '6',  4,  '4', 17, '17', 0.2463 * 80, '24.63%'],
     ]);
 
     var options = {
@@ -16,7 +16,7 @@ function drawChart() {
             0: {title: 'Thousands'},
             1: {title: 'Percentage', format: 'percent', viewWindow: {min: 0, max: 1}}
         },
-        hAxis: {title: ''},
+        hAxis: {title: 'Month'},
         seriesType: 'bars',
         series: {
             3: {type: 'line', targetAxisIndex: 0, lineWidth: 3, pointSize: 8, color: '#3498db', curveType: 'none'}
