40eacba1d2553032d940c0b63e858e427b6d76d9
ArrealdoRivaldi <ArrealdoRivaldi@users.noreply.github.com>
2025-05-28 16:23:53 +0800

    update

--- api/firestore.js
+++ api/firestore.js
@@ -17,7 +17,7 @@ const db = getFirestore();
 
 module.exports = async (req, res) => {
   try {
-    const snapshot = await db.collection('house_keeping').get();
+    const snapshot = await db.collection('fallout').get();
     const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
     res.status(200).json(data);
   } catch (error) {
