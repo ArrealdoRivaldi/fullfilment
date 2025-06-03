eff591ad8c616c017e5949065d5dbf453f573109
ArrealdoRivaldi <ArrealdoRivaldi@users.noreply.github.com>
2025-06-03 16:28:47 +0700

    update

---
+++ pages/hk/index.js
@@ -0,0 +1,8 @@
+export default function HK() {
+  return (
+    <div style={{padding:'2rem'}}>
+      <h1>Tool House Keeping (Next.js)</h1>
+      <a href="/" style={{color:'#3182ce',fontWeight:'bold'}}>Kembali ke Beranda</a>
+    </div>
+  );
+} 
