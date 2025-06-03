0fb690a26f5914cd8c1e9366d08b11b85f9410d0
ArrealdoRivaldi <ArrealdoRivaldi@users.noreply.github.com>
2025-06-03 16:34:51 +0700

    update

--- pages/hk/index.js
+++
@@ -1,8 +0,0 @@
-export default function HK() {
-  return (
-    <div style={{padding:'2rem'}}>
-      <h1>Tool House Keeping (Next.js)</h1>
-      <a href="/" style={{color:'#3182ce',fontWeight:'bold'}}>Kembali ke Beranda</a>
-    </div>
-  );
-} 
--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{padding:'2rem'}}>
-      <h1>Fullfilment FBB Dashboard (Next.js)</h1>
-      <a href="/hk" style={{color:'#3182ce',fontWeight:'bold'}}>Go to Tool House Keeping</a>
-    </div>
-  );
-} 
