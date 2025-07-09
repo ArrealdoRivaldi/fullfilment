36d3d743a17ab25515dc2f6aac097c3a577b03bd
ArrealdoRivaldi <ArrealdoRivaldi@users.noreply.github.com>
2025-07-09 14:28:19 +0800

    update

--- js/hk.js
+++ js/hk.js
@@ -9,7 +9,8 @@ const statusHKOptions = [
     { value: "PT3", label: "PT3" },
     { value: "Reorder", label: "Reorder" },
     { value: "Revoke", label: "Revoke" },
-    { value: "Offering Orbit", label: "Offering Orbit" }
+    { value: "Offering Orbit", label: "Offering Orbit" },
+    { value: "UNSC", label: "UNSC" }
 ];
 let userNop = null;
 let filteredByNopData = [];
@@ -92,6 +93,13 @@ function initializeFilters(data) {
     optNotYet.value = 'notyet';
     optNotYet.textContent = 'Not Yet';
     statusHKSelect.appendChild(optNotYet);
+    // Tambahkan semua opsi dari statusHKOptions
+    statusHKOptions.forEach(opt => {
+        const option = document.createElement('option');
+        option.value = opt.value;
+        option.textContent = opt.label;
+        statusHKSelect.appendChild(option);
+    });
 }
 function hitungAgingHari(provi_ts) {
     try {
